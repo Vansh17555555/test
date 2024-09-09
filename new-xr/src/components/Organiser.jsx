f8cc6061c90e5faa242dd6af527522bc5b82654c
Vansh Verma <vvansh739@gmail.com>
2024-09-09 23:39:26 +0530

    Update Organiser.jsx

--- new-xr/src/components/Organiser.jsx
+++ new-xr/src/components/Organiser.jsx
@@ -10,13 +10,13 @@ return (
       <h2 className="organizers-title">ORGANIZERS</h2>
       <div className="organizers-logos">
         <div className="organizer-item">
-          <img src="/assets/Group 6181.jpg" alt="XDG" className="organizer-logo" />
+          <img src="/assets/Group 6181.jpg" alt="XDG" className="organizer-logo" style={{height:'4vw' ,width:'7vw' }}/>
         </div>
         <div className="organizer-item">
-          <img src="/assets/image 3(1).jpg" alt="Wavelaps" className="organizer-logo" />
+          <img src="/assets/image 3(1).jpg" alt="Wavelaps" className="organizer-logo" style={{height:'3.5vw' ,width:'13vw' }}/>
         </div>
         <div className="organizer-item">
-          <img src="/assets/Group 6340(1).jpg" alt="BharatXR" className="organizer-logo" />
+          <img src="/assets/Group 6340(1).jpg" alt="BharatXR" className="organizer-logo" style={{height:'3.3vw' ,width:'13vw' }}/>
         </div>
       </div>
     </div>
