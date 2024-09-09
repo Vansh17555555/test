0141ef57cf7c0ee5e92541db3c8d3ced411624f9
Vansh Verma <vvansh739@gmail.com>
2024-09-09 22:47:24 +0530

    Update App.jsx

--- new-xr/src/App.jsx
+++ new-xr/src/App.jsx
@@ -1,4 +1,4 @@
-//import React from 'react';
+import  { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import FAQ from './components/FAQ';
@@ -10,9 +10,24 @@ import Prize from './components/Prize';
 import Organiser from './components/Organiser';
 import Waves from './components/Waves';
 import Sponsors from './components/Sponsors';
-
+import Loader from './components/Loader';
+import Marquee from './components/Marquee';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    window.addEventListener('load', handleLoad);
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
   const register = () => {
     alert(
       "We're excited to announce that registrations will begin on 10th September. Mark your calendars and stay tuned for updates. We can't wait to see you join us!"
@@ -21,18 +36,19 @@ const App = () => {
 
   return (
     <>
+      {loading && <Loader />}
       <Navbar register={register} />
       <section id="launchpage" className="pages">
-            <img src="/assets/Banner Layers.png" alt=""/>
-        </section>
+        <img src="/assets/Banner Layers.png" alt=""/>
+        <Marquee/>
+      </section>
+    
       <section id="about">
         <About />
-        </section>
-        
-        <section>
+      </section>
+      <section>
         <Waves/>
       </section>
-      
       <section id="theme">
         <Theme />
       </section>
